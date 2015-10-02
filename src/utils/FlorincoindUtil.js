ab784724f41e3bceadf9d23ebbaf73d90f2bac58
Luigi Poole <luigipoole@outlook.com>
2015-10-01 20:42:43 -0700

    handle prexsisting Florincoind configs

--- src/utils/FlorincoindUtil.js
+++ src/utils/FlorincoindUtil.js
@@ -7,6 +7,7 @@ import remote from 'remote';
 import Settings from '../utils/SettingsUtil';
 import nodeUtil from 'util';
 import fs from 'fs';
+import readline from 'readline';
 
 let dialog = remote.require('dialog');
 let app = remote.require('app');
@@ -31,53 +32,82 @@ module.exports = {
                     console.log(exists);
                     if (!exists)
                         var needsConf = true
+                    else
+                        var Confexsist = true;
                     if (!Settings.get('Florincoind-username') || !Settings.get('Florincoind-password'))
                         var needsConf = true;
-                    else
+                    if ((Settings.get('Florincoind-username') && Settings.get('Florincoind-password')) && !Confexsist)
                         var justsave = true;
+                    if (Confexsist && needsConf)
+                        var importConf = true;
                     console.log(needsConf, justsave, path.join(app.getPath('appData'), 'Florincoin', 'Florincoin.conf'))
                     if (needsConf) {
-                        var AutoGenPass = util.generatePassword(125);
-                        console.log(AutoGenPass);
-                        Settings.save('Florincoind-username', 'admin');
-                        Settings.save('Florincoind-password', AutoGenPass);
                         var FlorincoinTmp = path.join(app.getPath('appData'), 'Florincoin');
-                        if (!justsave) {
-                            dialog.showMessageBox({
-                                title: 'Alexandria-Librarian: Information',
-                                message: 'No RPC username/password found for Florincoin Wallet',
-                                detail: 'Username & password will be auto generated. See preferences for more info.',
-                                buttons: ['OK']
+                        var FlorincoinConf = path.join(FlorincoinTmp, 'florincoin.conf');
+                        if (!importConf) {
+                            var AutoGenPass = util.generatePassword(125);
+                            console.log(AutoGenPass);
+                            Settings.save('Florincoind-username', 'admin');
+                            Settings.save('Florincoind-password', AutoGenPass);
+                            var FlorincoinTmp = path.join(app.getPath('appData'), 'Florincoin');
+                            if (!justsave) {
+                                dialog.showMessageBox({
+                                    title: 'Alexandria-Librarian: Information',
+                                    message: 'No RPC username/password found for Florincoin Wallet',
+                                    detail: 'Username & password will be auto generated. See preferences for more info.',
+                                    buttons: ['OK']
+                                });
+                            }
+                            util.createDir(FlorincoinTmp)
+                                .then(function() {
+                                    var config = [
+                                        'rpcserver=1',
+                                        'rpcallowip=192.168.0.*',
+                                        'rpcallowip=127.0.0.1',
+                                        'rpcport=7313',
+                                        'daemon=1',
+                                        'server=1',
+                                        'listen=1',
+                                        'port=7312',
+                                        'noirc=0',
+                                        'maxconnections=30',
+                                        'addnode=146.185.148.114',
+                                        'addnode=192.241.171.45',
+                                        nodeUtil.format('rpcuser=%s', Settings.get('Florincoind-username')),
+                                        nodeUtil.format('rpcpassword=%s', AutoGenPass)
+                                    ];
+                                    return new Promise((resolve, reject) => {
+                                        fs.writeFile(FlorincoinConf, config.join('\n'), function(err) {
+                                            if (err)
+                                                return reject(err);
+                                            resolve('Config saved sucsessfuly');
+                                        });
+                                    });
+                                })
+                                .then(resolve)
+                                .catch(reject);
+                        } else {
+                            var userfound = false;
+                            var userpassfound = false;
+                            readline.createInterface({
+                                input: require('fs').createReadStream('file.in')
+                            }).on('line', function(line) {
+                                line = line.split('=');
+                                if (line[0] === 'rpcuser') {
+                                    userfound = true;
+                                    Settings.save('Florincoind-username', line[1]);
+                                }
+                                if (line[0] === 'rpcpassword') {
+                                    userpassfound = true;
+                                    Settings.save('Florincoind-password', line[1]);
+                                }
+                            }).on('close', function() {
+                                if (userpassfound && userfound)
+                                    resolve()
+                                else
+                                    reject()
                             });
                         }
-                        util.createDir(FlorincoinTmp)
-                            .then(function() {
-                                var config = [
-                                    'rpcserver=1',
-                                    'rpcallowip=192.168.0.*',
-                                    'rpcallowip=127.0.0.1',
-                                    'rpcport=7313',
-                                    'daemon=1',
-                                    'server=1',
-                                    'listen=1',
-                                    'port=7312',
-                                    'noirc=0',
-                                    'maxconnections=30',
-                                    'addnode=146.185.148.114',
-                                    'addnode=192.241.171.45',
-                                    nodeUtil.format('rpcuser=%s', Settings.get('Florincoind-username')),
-                                    nodeUtil.format('rpcpassword=%s', AutoGenPass)
-                                ];
-                                return new Promise((resolve, reject) => {
-                                    fs.writeFile(path.join(FlorincoinTmp, 'florincoin.conf'), config.join('\n'), function(err) {
-                                        if (err)
-                                            return reject(err);
-                                        resolve('Config saved sucsessfuly');
-                                    });
-                                });
-                            })
-                            .then(resolve)
-                            .catch(reject);
                     } else
                         resolve();
                 })
