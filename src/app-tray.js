247913e364245f6b09ea5192e0787fd626d4beeb
Luigi Poole <luigipoole@outlook.com>
2015-10-12 20:45:49 -0700

    Move tray icons into images/tray and add osx to filename, reduce code in app-tray.js

--- src/app-tray.js
+++ src/app-tray.js
@@ -3,35 +3,29 @@ import MenuItem from 'menu-item';
 import Tray from 'tray';
 import ipc from 'ipc';
 import path from 'path';
+import util from './utils/Util';
 
 // Define a function to set up our tray icon
 exports.init = function(helper) {
 
-	// Disconnected State
-	var trayMenu = new Menu();
-	trayMenu.append(new MenuItem({
-		label: 'Toggle ΛLΞXΛNDRIΛ Librarian',
-		click: helper.toggleVisibility
-	}));
-	trayMenu.append(new MenuItem({
-		type: 'separator'
-	}));
-	
-	trayMenu.append(new MenuItem({
-		label: 'Quit',
-		click: helper.quit
-	}));
+    // Disconnected State
+    var trayMenu = new Menu();
+    trayMenu.append(new MenuItem({
+        label: 'Toggle ΛLΞXΛNDRIΛ Librarian',
+        click: helper.toggleVisibility
+    }));
+    trayMenu.append(new MenuItem({
+        type: 'separator'
+    }));
 
+    trayMenu.append(new MenuItem({
+        label: 'Quit',
+        click: helper.quit
+    }));
 
-	let trayImage = path.normalize(path.join(__dirname, '../', 'images/icons/tray.png'));
+    var tray = new Tray(path.normalize(path.join(__dirname, '../', 'images/icons/tray/tray_' + util.getOS() + '.png')));
+    tray.setContextMenu(trayMenu);
 
-	var tray = new Tray(trayImage);
-	tray.setContextMenu(trayMenu);
-
-	tray.on('clicked', helper.toggleVisibility);
-
-
-
-	
+    tray.on('clicked', helper.toggleVisibility);
 
 };
