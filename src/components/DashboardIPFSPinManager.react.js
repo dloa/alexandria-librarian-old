2d7db5ecc24bbe9429ece1dc02e4de894382c2eb
Luigi Poole <luigipoole@outlook.com>
2015-10-13 20:42:25 -0700

    implement scrollable view for ipfs pinned and remove table view dep

--- src/components/DashboardIPFSPinManager.react.js
+++ src/components/DashboardIPFSPinManager.react.js
@@ -1,31 +1,22 @@
 import React from 'react/addons';
 import Router from 'react-router';
-import LogStore from '../stores/LogStore';
 import IPFS from '../actions/ipfsActions';
-import TableView from 'react-table-view'
 
-
-
-// must ensure all of your fields have values 
-let PINNEDDATA = [{
+var PINNEDDATA = [{
     hash: ''
 }];
 
 var PinManager = React.createClass({
-
     getInitialState: function() {
         return {
             pinned: PINNEDDATA,
             enteredHash: null
         };
     },
-
     componentDidMount: function() {
         IPFS.getPinned()
             .then(this.update);
     },
-
-
     update: function(newhash) {
         if (this.isMounted()) {
             this.setState({
@@ -33,46 +24,37 @@ var PinManager = React.createClass({
             });
         }
     },
-
     handleHashInput: function(event) {
         this.setState({
             enteredHash: event.target.value
         });
     },
     handleAddPinLocal: function() {
-
         IPFS.pinlocal();
-
     },
     handleAddPinHash: function() {
-
-        console.log(this.state.enteredHash)
         var remotehash = this.state.enteredHash
         IPFS.pinRemote(remotehash);
     },
     handlePinRefresh: function() {
         var self = this;
-
         IPFS.getPinned()
             .then(function(data) {
                 self.update(data);
             });
-
-
     },
     render: function() {
-
         return (
-        <section>
-            <h1 className="title">IPFS pin manager</h1>
-            <TableView data={this.state.pinned}  />
-            <br></br>
-            <input name="username"  onChange={this.handleHashInput} placeholder="Remote Hash" type="text" />
-            <br></br>
-            <button className="left" type="submit" onClick={this.handleAddPinLocal}><p>Pin local File</p></button>
-             <button className="left" type="submit" onClick={this.handleAddPinHash}><p>Pin hash</p></button>
-            <button className="left" type="submit" onClick={this.handlePinRefresh}><p>Refresh all pins</p></button>
-        </section>
+            <section>
+                <h1 className="title">IPFS pin manager</h1>
+                <textarea className="logs" name="description" value={this.state.pinned} readOnly />
+                    <br></br>
+                <input name="username"  onChange={this.handleHashInput} placeholder="Remote Hash" type="text" />
+                    <br></br>
+                <button className="left" type="submit" onClick={this.handleAddPinLocal}><p>Pin local File</p></button>
+                <button className="left" type="submit" onClick={this.handleAddPinHash}><p>Pin hash</p></button>
+                <button className="left" type="submit" onClick={this.handlePinRefresh}><p>Refresh all pins</p></button>
+            </section>
         );
     }
 });
