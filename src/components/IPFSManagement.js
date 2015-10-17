8f864a9583b4e11adbb52720e90efb012dd8a26d
Luigi Poole <luigipoole@outlook.com>
2015-10-17 12:38:45 -0700

    make IPFS management component

---
+++ src/components/IPFSManagement.js
@@ -0,0 +1,49 @@
+import React from 'react/addons';
+import Router from 'react-router';
+import IPFS from '../actions/ipfsActions';
+import Logs from './DashboardLogs.react';
+import utils from '../utils/Util';
+
+
+let If = React.createClass({
+    render: function() {
+        if (this.props.test) {
+            return this.props.children;
+        } else {
+            return false;
+        }
+    }
+});
+
+
+var IPFSManagement = React.createClass({
+    mixins: [Router.Navigation],
+
+    getInitialState: function() {
+        return {
+        };
+    },
+    handleOpenIPFSWebUI: function(e) {
+
+    },
+    render: function() {
+        return (
+      <div className='content-scroller' id='content'>
+        <section className="ipfsStatus">
+            <p>Status:</p><span>Connected to 7 peers</span>
+        </section>
+        <section className="cli-emulator">
+            
+        </section>
+        <section className="ipfsPinManager">
+            
+        </section>
+      </div>
+        );
+
+    }
+
+});
+
+
+module.exports = IPFSManagement;
