97882698c7876a79463ba893be04d5b360069c21
Luigi Poole <luigipoole@outlook.com>
2015-10-19 01:15:04 -0700

    Start work on pinned hashes datagrid

--- src/components/DashboardIPFSPinManager.react.js
+++ src/components/DashboardIPFSPinManager.react.js
@@ -1,6 +1,54 @@
 import React from 'react/addons';
 import Router from 'react-router';
 import IPFS from '../actions/ipfsActions';
+import ReactDataGrid from 'react-data-grid';
+
+var rowGetter = function(i) {
+    return _rows[i];
+};
+
+var unpinFormatter = React.createClass({
+    render: function() {
+        return (<img width='75' src={this.props.value} />);
+    }
+});
+
+
+//Columns definition
+var columns = [{
+    key: 'filename',
+    name: 'file name',
+    width: 75
+}, {
+    key: 'hash',
+    name: 'hash',
+    width: 120
+}, {
+    key: 'filesize',
+    name: 'file size'
+}, {
+    key: 'seeds',
+    name: 'seeds'
+}, {
+    key: 'up-pin',
+    name: 'up-pin',
+    formatter: unpinFormatter
+}];
+
+
+var RowRenderer = React.createClass({
+    displayName: "RowRenderer",
+    getRowStyle: function() {
+        return {
+            height: '75px'
+        }
+    },
+    render: function() {
+        return (<div style={this.getRowStyle()}><ReactDataGrid.Row ref="row" {...this.props}/></div>)
+    }
+});
+
+
 
 var PinManager = React.createClass({
     getInitialState: function() {
