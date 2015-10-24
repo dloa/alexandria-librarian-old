fa64d4953449eb2b5df18c0a0085333a37e4fdbf
Luigi Poole <luigipoole@outlook.com>
2015-10-24 01:19:51 -0700

    Show enable no matter what

--- src/components/Dashboard.react.js
+++ src/components/Dashboard.react.js
@@ -52,29 +52,6 @@ var Preferences = React.createClass({
         }
     },
 
-    InstallLibraryd: function() {
-        Libraryd.install()
-
-        this.setState({
-            LibrarydInstalled: true
-        });
-
-    },
-    InstallIPFS: function() {
-
-        IPFS.install()
-        this.setState({
-            IPFSInstalled: true
-        });
-
-
-    },
-    InstallFlorincoind: function() {
-        Florincoind.install();
-        this.setState({
-            FlorincoindInstalled: true
-        });
-    },
     handleChangeFlorincoindEnabled: function(e) {
         var checked = e.target.checked;
         Florincoind.toggle(checked);
@@ -87,7 +64,6 @@ var Preferences = React.createClass({
         this.setState({
             LibrarydEnabled: checked
         });
-
         Libraryd.toggle(checked);
     },
     handleChangeIPFSEnabled: function(e) {
@@ -106,46 +82,31 @@ var Preferences = React.createClass({
                 <section>
                     <h1 className='title'>Local Daemons</h1>
                     <div className="DaemonWrapper">
-                        <If test={this.state.LibrarydInstalled}>
-                            <div className="toggle-wrapper">
-                                <input checked={this.state.LibrarydEnabled} onChange={this.handleChangeLibrarydEnabled} type="checkbox" id="LibrarydToggle" className="toggle" />
-                                <label htmlFor="LibrarydToggle"></label>
-                            </div>
-                        </If>
+                        <div className="toggle-wrapper">
+                            <input checked={this.state.LibrarydEnabled} onChange={this.handleChangeLibrarydEnabled} type="checkbox" id="LibrarydToggle" className="toggle" />
+                            <label htmlFor="LibrarydToggle"></label>
+                        </div>
                         <p>Libraryd</p>
                         <i className="ion-information-circled"/>
-                        <If test={!this.state.LibrarydInstalled}>
-                            <div onClick={this.InstallLibraryd} className="install">install</div>
-                        </If>
                     </div>
                     <div className="DaemonWrapper">
-                        <If test={this.state.IPFSInstalled}>
-                            <div className="toggle-wrapper">
-                                <input checked={this.state.IPFSEnabled} onChange={this.handleChangeIPFSEnabled} type="checkbox" id="IPFStoggle" className="toggle" />
-                                <label htmlFor="IPFStoggle"></label>
-                            </div>
-                        </If>
+                        <div className="toggle-wrapper">
+                            <input checked={this.state.IPFSEnabled} onChange={this.handleChangeIPFSEnabled} type="checkbox" id="IPFStoggle" className="toggle" />
+                            <label htmlFor="IPFStoggle"></label>
+                        </div>
                         <p>IPFS</p>
                         <i className="ion-information-circled"/>
                         <If test={this.state.IPFSEnabled}>
                             <i data-tip="Open IPFS Web Interface" onClick={this.handleOpenIPFSWebUI} className="ion-cube"/>
                         </If>
-                        <If test={!this.state.IPFSInstalled}>
-                            <div onClick={this.InstallIPFS} className="install">install</div>
-                        </If>
                     </div>
                     <div className="DaemonWrapper">
-                        <If test={this.state.FlorincoindInstalled}>
-                            <div className="toggle-wrapper">
-                                <input checked={this.state.FlorincoindEnabled} onChange={this.handleChangeFlorincoindEnabled} type="checkbox" id="Florincoindtoggle" className="toggle" />
-                                <label htmlFor="Florincoindtoggle"></label>
-                            </div>
-                        </If>
+                        <div className="toggle-wrapper">
+                            <input checked={this.state.FlorincoindEnabled} onChange={this.handleChangeFlorincoindEnabled} type="checkbox" id="Florincoindtoggle" className="toggle" />
+                            <label htmlFor="Florincoindtoggle"></label>
+                        </div>
                         <p>Florincoin</p>
                         <i className="ion-information-circled"/>
-                        <If test={!this.state.FlorincoindInstalled}>
-                            <div onClick={this.InstallFlorincoind} className="install">install</div>
-                        </If>
                     </div>
                 </section>
                 <Logs />
