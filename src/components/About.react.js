639bfd4346384040ddb2265c482e4859ccd00f09
Conor Maguire <conormag94@gmail.com>
2015-10-25 21:02:02 +0000

    Standardise spelling of "license"

--- src/components/About.react.js
+++ src/components/About.react.js
@@ -10,30 +10,30 @@ var About = React.createClass({
     getInitialState: function() {
         return {
             contributors: externalStore.getState().contributors,
-            lisence: externalStore.getState().lisence,
+            license: externalStore.getState().license,
             version: externalStore.getState().version
         }
     },
     componentDidMount: function() {
-        if (!this.state.lisence || !this.state.contributors || !this.state.version) {
+        if (!this.state.license || !this.state.contributors || !this.state.version) {
             externalStore.listen(this.update);
             if (!this.state.version)
                 externalActions.getVersion();
-            if (!this.state.lisence)
-                externalActions.getLisence();
+            if (!this.state.license)
+                externalActions.getLicense();
             if (!this.state.contributors)
                 externalActions.getContributors();
         }
     },
     componentWillUnmount: function() {
-        if (!this.state.lisence || !this.state.contributors || !this.state.version)
+        if (!this.state.license || !this.state.contributors || !this.state.version)
             externalStore.unlisten(this.update);
     },
     update: function() {
         if (this.isMounted()) {
             this.setState({
                 contributors: externalStore.getState().contributors,
-                lisence: externalStore.getState().lisence,
+                license: externalStore.getState().license,
                 version: externalStore.getState().version
             });
         }
@@ -45,7 +45,7 @@ var About = React.createClass({
 
     render: function() {
         var contributors = this.state.contributors ? this.state.contributors : [];
-        var lisence = this.state.lisence ? this.state.lisence : 'Loading...';
+        var license = this.state.license ? this.state.license : 'Loading...';
         var version = this.state.version ? this.state.version : 'Loading...';
         return (
             <div className="content-scroller" id="content">
@@ -57,17 +57,17 @@ var About = React.createClass({
                 </section>
                 <section>
                     <h1 className="title">Contributors</h1>
-                        { 
+                        {
                             contributors.map(function(Contributor, i) {
                                 return (
                                         <p className="Contributor">{Contributor.name} {Contributor.email} <i data-github={Contributor.github}  onClick={this.openGithub} className="ion-social-github" /></p>
                                     );
                                 }, this)
-                        }             
+                        }
                  </section>
                 <section>
                     <h1 className="title">License</h1>
-                    <textarea className="License"  defaultValue={lisence} readOnly />
+                    <textarea className="License"  defaultValue={license} readOnly />
                 </section>
             </div>
         );
