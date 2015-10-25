b6790f757d84c1dbe0f8fa92e2398793d66f55ce
Conor Maguire <conormag94@gmail.com>
2015-10-25 23:40:38 +0000

    Add beginnings of updater functions in updaterUtil.js

--- src/utils/updaterUtil.js
+++ src/utils/updaterUtil.js
@@ -1,8 +1,20 @@
 import util from './util';
-import IPFS from '../actions/ipfsActions';
+import ipfsUtil from './ipfsUtil';
+import updateActions from '../actions/updateActions';
 
 module.exports = {
     checkForUpdates: function() {
         console.log("updaterUtil checkForUpdates called!!!");
+        ipfsUtil.cli("ipfs cat Qme1JTA5JnRM64CAyn4uLmGXhuiZg5Zhae5J4aKa86aMKx");
+    },
+
+    checkMainUpdate: function() {
+        var mainUpdate = {};
+        updateActions.mainUpdateFound('mainUpdate');
+    },
+
+    checkDaemonUpdates: function() {
+        var daemonUpdates = [];
+        updateActions.daemonUpdatesFound('daemonUpdates');
     }
 }
