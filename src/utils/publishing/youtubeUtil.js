cf9bc346002b6a8b8ebc93252c74b6b47a21afcd
Luigi Poole <luigipoole@outlook.com>
2015-10-26 13:48:23 -0700

    beginnings of youtube publish Util

---
+++ src/utils/publishing/youtubeUtil.js
@@ -0,0 +1,19 @@
+import Promise from 'bluebird';
+
+
+
+module.exports = {
+    signIn: function(creds) {
+        return new Promise((resolve, reject) => {
+
+        });
+    },
+    download: function() {
+        return new Promise((resolve, reject) => {
+
+        });
+    },
+    getAll: function(){
+      
+    }
+}
