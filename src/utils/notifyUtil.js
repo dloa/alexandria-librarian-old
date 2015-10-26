0549990ecbbe03e0451b40738b598600961bfae8
Luigi Poole <luigipoole@outlook.com>
2015-10-25 21:26:19 -0700

    remove excess whitespace from notify util

--- src/utils/notifyUtil.js
+++ src/utils/notifyUtil.js
@@ -4,20 +4,15 @@ import _ from 'lodash';
 
 module.exports = {
     notify: function(notifyparams, clickFunction) {
-
         notifyparams = _.defaults(notifyparams, {
             icon: path.join(__dirname, '../../', 'images/icons/logo.png'),
             message: '',
             sound: false,
             wait: false
         });
-
         notifier.notify(notifyparams);
-        
         if (clickFunction)
             notifier.on('click', clickFunction);
-
-
     }
 
 };
