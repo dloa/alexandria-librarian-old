7de40cab6739de81caac112f407fd668fa317539
Conor Murphy <murphc94@tcd.ie>
2015-10-26 00:35:02 +0000

    Initial progress in updaterUtil.js

--- src/utils/updaterUtil.js
+++ src/utils/updaterUtil.js
@@ -5,7 +5,10 @@ import updateActions from '../actions/updateActions';
 module.exports = {
     checkForUpdates: function() {
         console.log("updaterUtil checkForUpdates called!!!");
-        ipfsUtil.cli("ipfs cat Qme1JTA5JnRM64CAyn4uLmGXhuiZg5Zhae5J4aKa86aMKx");
+        var currentVersion = require("../../package.json").version;
+        console.log(currentVersion);
+        ipfsUtil.cli(['cat', 'Qme1JTA5JnRM64CAyn4uLmGXhuiZg5Zhae5J4aKa86aMKx']);
+        
     },
 
     checkMainUpdate: function() {
