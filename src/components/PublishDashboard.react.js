0c17788f5a743f0c7d39c03c36cbd030ba7ad9f9
OstlerDev <ostlerdev@me.com>
2015-11-22 16:07:17 -0700

    Get UI displaying mostly correct

--- src/components/PublishDashboard.react.js
+++ src/components/PublishDashboard.react.js
@@ -33,128 +33,130 @@ default React.createClass({
     },
     render() {
         return (
-            <div class="container">
+            <div className="content-scroller">
+            <div className="container">
             <h2>Publish Artifact</h2>
             <h3>Choose Artifact Type</h3>
-            <div class="well full">
-                <div class="clear"></div>
-                <div class="artifactModule">
-                    <div class="artifactCircle archive"></div>
-                    <p class="artifactText">Archive</p>
+            <div className="well full">
+                <div className="clear"></div>
+                <div className="artifactModule">
+                    <div className="artifactCircle archive"></div>
+                    <p className="artifactText">Archive</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle movie"></div>
-                    <p class="artifactText">Movie</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle movie"></div>
+                    <p className="artifactText">Movie</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle video"></div>
-                    <p class="artifactText">Video</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle video"></div>
+                    <p className="artifactText">Video</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle song"></div>
-                    <p class="artifactText">Song</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle song"></div>
+                    <p className="artifactText">Song</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle album active"></div>
-                    <p class="artifactText">Album</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle album active"></div>
+                    <p className="artifactText">Album</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle podcast"></div>
-                    <p class="artifactText">Podcast</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle podcast"></div>
+                    <p className="artifactText">Podcast</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle recipies"></div>
-                    <p class="artifactText">Recipies</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle recipies"></div>
+                    <p className="artifactText">Recipies</p>
                 </div>
-                <div class="artifactModule">
-                    <div class="artifactCircle things"></div>
-                    <p class="artifactText">Things</p>
+                <div className="artifactModule">
+                    <div className="artifactCircle things"></div>
+                    <p className="artifactText">Things</p>
                 </div>
-                <div class="clear"></div>
+                <div className="clear"></div>
             </div>
-            <div class="twothirds left">
+            <div className="twothirds left">
                 <h3>Album Information</h3>
-                <div class="well">
-                    <div class="pad">
-                        <div class="inputSpacing">
-                            <p class="albumText">Artist Name</p> 
+                <div className="well">
+                    <div className="pad">
+                        <div className="inputSpacing">
+                            <p className="albumText">Artist Name</p> 
                             <input type="text" name="artist-name"></input>
                         </div>
-                        <div class="clear"></div>
-                        <div class="inputSpacing">
-                            <p class="albumText">Album Title</p> 
+                        <div className="clear"></div>
+                        <div className="inputSpacing">
+                            <p className="albumText">Album Title</p> 
                             <input type="text" name="album-title"></input>
                         </div>
-                        <div class="clear"></div>
-                        <div class="inputSpacing">
-                            <p class="albumText">Record Label</p> 
+                        <div className="clear"></div>
+                        <div className="inputSpacing">
+                            <p className="albumText">Record Label</p> 
                             <input type="text" name="record-label"></input>
                         </div>
-                        <div class="clear"></div>
-                        <div class="inputSpacing">
-                            <p class="albumText">Release Date</p> 
+                        <div className="clear"></div>
+                        <div className="inputSpacing">
+                            <p className="albumText">Release Date</p> 
                             <input type="date" name="release-date"></input>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="onethird right">
+            <div className="onethird right">
                 <h3>Cover Art</h3>
-                <div class="well album-artwork" id="artwork">
-                    <div class="album-image" id="album-image"></div>
-                    <h2 class="cover-text">drag cover art file here</h2>
+                <div className="well album-artwork" id="artwork">
+                    <div className="album-image" id="album-image"></div>
+                    <h2 className="cover-text">drag cover art file here</h2>
                 </div>
             </div>
-            <div class="onefourth left">
+            <div className="onefourth left">
                 <h3>Individual Track Pricing</h3>
-                <div class="well">
-                    <div class="pricing-pad">
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Suggested price / play </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                <div className="well">
+                    <div className="pricing-pad">
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Suggested price / play </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Minimum price / play </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Minimum price / play </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Suggested price / download </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Suggested price / download </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Minimum price / download </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Minimum price / download </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="onefourth left">
+            <div className="onefourth left">
                 <h3>Entire Album Pricing</h3>
-                <div class="well">
-                    <div class="pricing-pad">
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Suggested price / play </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                <div className="well">
+                    <div className="pricing-pad">
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Suggested price / play </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Minimum price / play </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Minimum price / play </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Suggested price / download </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Suggested price / download </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
-                        <div class="pricingSpacing">
-                            <span class="price-width left">Minimum price / download </span><input id="price1" class="pricing" type="text" name="single-play-price" value="$"></input>
+                        <div className="pricingSpacing">
+                            <span className="price-width left">Minimum price / download </span><input id="price1" className="pricing" type="text" name="single-play-price" value="$"></input>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="full left">
+            <div className="full left">
                 <h3>Audio Tracks</h3>
-                <div class="well">
-                    <h2 class="audio-text">drag audio files here</h2>
+                <div className="well">
+                    <h2 className="audio-text">drag audio files here</h2>
                 </div>
             </div>
-            <div class="full left">
+            <div className="full left">
                 <h3>Extra Files</h3>
-                <div class="well">
-                    <h2 class="audio-text">drag extra files here</h2>
+                <div className="well">
+                    <h2 className="audio-text">drag extra files here</h2>
                 </div>
             </div>
+        </div>
         </div>
         );
     }
