995e45882017215e644cb2c6184240b21d7c6e5e
OstlerDev <ostlerdev@me.com>
2015-11-22 23:47:09 -0700

    Get audio table kinda working

--- src/components/PublishDashboard.react.js
+++ src/components/PublishDashboard.react.js
@@ -8,6 +8,10 @@ import publishStore from '../stores/publishStore';
 
 var Dropzone = require('react-dropzone');
 
+var artwork = "";
+var audioFiles = [];
+var extraFiles = [];
+
 
 let If = React.createClass({
     render() {
@@ -30,6 +34,47 @@ default React.createClass({
       console.log(this.refs.artwork);
       this.refs.artwork.style.backgroundImage = "url('file://" + files[0].path + "')";
       this.refs.albumText.innerHTML = "";
+    },
+
+    audioDrop: function (files) {
+      console.log('Received files: ', files);
+      for (var i = 0; i < files.length; i++) {
+          if (files[i].type.indexOf("audio") > -1){
+            if (audioFiles.length == 0){
+                this.refs.audioInner.innerHTML = "<div class='pad'><table><thead><tr><th style='width:10%'>File #</th><th style='width:40%' class='left-text'>File Name</th><th style='width:15%'>File Size</th><th style='width:10%'>Duration</th><th style='width:10%'>Track #</th><th style='width:25%' class='left-text'>Display Name</th></tr></thead><tbody><tr><td>File #"+(audioFiles.length+files.length)+"</td><td class='left-text'>" + files[i].name + "</td><td>3.5 MB</td><td>3:27</td><td><input type='text' value='1'></input></td><td class='left-text'><input type='text' value='DJ Day' style='width:90%'></input></td></tr></tbody></table></div>";
+                /*<div className='pad'>
+                        <table>
+                          <thead>
+                            <tr>
+                              <th style='width: 10%' >File #</th>
+                              <th style='width: 40%' class='left-text'>File Name</th>
+                              <th style='width: 15%'>File Size</th>
+                              <th style='width: 10%'>Duration</th>
+                              <th style='width: 10%'>Track #</th>
+                              <th style='width: 25%' class='left-text'>Display Name</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>File #" + (audioFiles.length+files.length) + "</td>
+                              <td class='left-text'>" + files[i].name + "</td>
+                              <td>3.5 MB</td>
+                              <td>3:27</td>
+                              <td><input type='text' value='1'></input></td>
+                              <td class='left-text'><input type='text' value='DJ Day' style='width: 90%'></input></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                    </div>"*/
+            }
+          } else {
+            console.log("Not Audio!");
+          }
+      };
+    },
+
+    extraDrop: function (files) {
+      console.log('Received files: ', files);
     },
 
     update() {
@@ -156,15 +201,17 @@ default React.createClass({
             </div>
             <div className="full left">
                 <h3>Audio Tracks</h3>
-                <div className="well">
-                    <h2 className="audio-text">drag audio files here</h2>
-                </div>
+                <Dropzone className="well" activeClassName="well-dashed" ref="audio" onDrop={this.audioDrop}>
+                    <div ref="audioInner">
+                        <h2 className="audio-text">drag audio files here</h2>
+                    </div>
+                </Dropzone>
             </div>
             <div className="full left">
                 <h3>Extra Files</h3>
-                <div className="well">
+                <Dropzone className="well" activeClassName="well-dashed" ref="extraFiles" onDrop={this.extraDrop}>
                     <h2 className="audio-text">drag extra files here</h2>
-                </div>
+                </Dropzone>
             </div>
         </div>
         </div>
