bd6450bd6d2044009653b989e3789cb96deb9f84
Luigi Poole <luigipoole@outlook.com>
2015-11-29 21:34:21 -0800

    purge the gene pool

--- src/app-tray.js
+++ src/app-tray.js
@@ -2,7 +2,6 @@ import Menu from 'menu';
 import MenuItem from 'menu-item';
 import Tray from 'tray';
 import path from 'path';
-import util from './utils/util';
 
 // Define a function to set up our tray icon
 exports.init = helper => {
@@ -22,7 +21,7 @@ exports.init = helper => {
         click: helper.quit
     }));
 
-    var tray = new Tray(path.normalize(path.join(__dirname, '../', 'images/icons/tray/tray_' + util.getOS() + '.png')));
+    var tray = new Tray(path.normalize(path.join(__dirname, '../', 'images/icons/tray/tray_win.png')));
     tray.setContextMenu(trayMenu);
 
     tray.on('clicked', helper.toggleVisibility);
