617ab4d20b1ff41500e5c6473f41de3daedb2aca
Luigi Poole <luigipoole@outlook.com>
2015-11-30 20:05:54 -0800

    better stats

--- src/utils/daemon/ipfs.js
+++ src/utils/daemon/ipfs.js
@@ -4,25 +4,25 @@ import DaemonEngineStore from '../../stores/daemonEngineStore';
 import CommonUtil from '../../utils/CommonUtil';
 
 const getPeers = () => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         DaemonEngineStore.getState().enabled.ipfs.api.swarm.peers((err, output) => {
-            resolve(err ? err : output.Strings)
+            err ? reject(err) : resolve(output.Strings);
         });
     });
 }
 
 const getPinned = () => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         DaemonEngineStore.getState().enabled.ipfs.api.pin.list((err, output) => {
-            resolve(err ? err : output.Keys);
+            err ? reject(err) : resolve(output.Keys);
         });
     });
 }
 
 const sendCommand = (cmd, key = null, opts = {}) => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         DaemonEngineStore.getState().enabled.ipfs.api.send(cmd, key, opts, null, (err, output) => {
-            resolve(err ? err : output[0]);
+            err ? reject(err) : resolve(output[0]);
         });
     });
 }
@@ -30,27 +30,30 @@ const sendCommand = (cmd, key = null, opts = {}) => {
 module.exports = {
     refreshStats() {
         return new Promise(resolve => {
-            Promise.all([getPeers(), sendCommand('stats/bw'), getPinned()]).spread((peers, bandwidth, pinned) => {
-                resolve({
-                    id: 'ipfs',
-                    key: 'stats',
-                    stats: {
-                        peers: peers.length,
-                        pinned: {
-                            size: 0,
-                            total: pinned.length
-                        },
-                        speed: {
-                            up: CommonUtil.formatBytes(bandwidth.RateOut, 2) + '/s',
-                            down: CommonUtil.formatBytes(bandwidth.RateIn, 2) + '/s'
-                        },
-                        bw: {
-                            up: CommonUtil.formatBytes(bandwidth.TotalOut, 1),
-                            down: CommonUtil.formatBytes(bandwidth.TotalIn, 1)
+            Promise.all([getPeers(), sendCommand('stats/bw'), getPinned()])
+                .spread((peers, bandwidth, pinned) => {
+                    resolve({
+                        id: 'ipfs',
+                        key: 'stats',
+                        stats: {
+                            peers: peers.length,
+                            pinned: {
+                                size: 0,
+                                total: Object.keys(pinned).length
+                            },
+                            speed: {
+                                up: CommonUtil.formatBytes(bandwidth.RateOut.toFixed(3), 2) + '/s',
+                                down: CommonUtil.formatBytes(bandwidth.RateIn.toFixed(3), 2) + '/s'
+                            },
+                            bw: {
+                                up: CommonUtil.formatBytes(bandwidth.TotalOut.toFixed(3), 1),
+                                down: CommonUtil.formatBytes(bandwidth.TotalIn.toFixed(3), 1)
+                            }
                         }
-                    }
-                })
-            });
+                    })
+                }).catch(err => {
+                    console.error('IPFS refreshStats()', err);
+                });
         });
     },
     pin() {
