579816bf92b4d8a968c186652add77045a1a4c39
Luigi Poole <luigipoole@outlook.com>
2015-12-01 18:16:52 -0800

    submit shenanagins

--- src/components/Management/ipfs/pins.js
+++ src/components/Management/ipfs/pins.js
@@ -30,18 +30,20 @@ default React.createClass({
         });
     },
     handelPin(event) {
-
+        console.log('pin called')
     },
-
+    handelBrowseLocal() {
+        console.log('pin called')
+    },
     render() {
         return (
             <div className="section ipfs">
                 <form onSubmit={this.handelPin} className="form-inline">
-                    <button type="" className="btn btn-default">Browse local</button>
+                    <button onClick={this.handelBrowseLocal} type="button" className="btn btn-default">Browse local</button>
                     <p>or</p>
                     <div className="form-group pull-right">
                         <input ref="pin-hash" type="text" className="form-control ipfs" id="" placeholder="Enter Local Path, IPFS hash or URL"/>
-                        <button type="" className="btn btn-primary btn-pin-file">Pin file</button>
+                        <button type="submit" className="btn btn-primary btn-pin-file">Pin file</button>
                     </div>
                 </form>
                 
