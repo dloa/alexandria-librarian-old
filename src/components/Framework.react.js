47d9ed0ba9b5472617fd790c0f8c44727c2b7343
Luigi Poole <luigipoole@outlook.com>
2016-01-02 15:53:18 -0800

    check for updates on start

--- src/components/Framework.react.js
+++ src/components/Framework.react.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Router from 'react-router';
 import Sidebar from './Sidebar.react';
+import UpdaterUtil from '../utils/updaterUtil';
 
 var Client = React.createClass({
     getInitialState() {
         return {
             sidebarOffset: 0
         };
+    },
+    componentDidMount() {
+       UpdaterUtil.check();
     },
     handleScroll(e) {
         if (e.target.scrollTop > 0 && !this.state.sidebarOffset) {
