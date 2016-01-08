6e0fca924f005ae1a2d1d59ed60337a7b5c1dc06
Luigi Poole <luigipoole@outlook.com>
2016-01-07 18:00:36 -0800

    work on getting ipfs http api working once again

--- src/utils/httpAPIUtil.js
+++ src/utils/httpAPIUtil.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import _ from 'lodash';
 import request from 'request';
 import Preferences from './PreferencesUtil';
@@ -9,8 +10,16 @@ class HttpAPI extends Preferences {
         super();
 
         this.logs = [];
-
         this._api = express();
+        this.loadedExtensions = [];
+
+        this._APIRouter = express.Router();
+        this._APIRouter.get('/', (req, res) => res.json({
+            status: 'Librarian API online',
+            'available-daemons': this.loadedExtensions
+        }));
+
+        this._api.use('/api', this._APIRouter);
         this._api.use(morgan('combined', {
             stream: {
                 write: str => {
@@ -19,12 +28,21 @@ class HttpAPI extends Preferences {
                 }
             }
         }));
+        this._api.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        this._api.use(bodyParser.json());
 
+        this._api.get('/', (req, res) => res.json({
+            paths: ['/api/<Daemon>/<action>/<command>/<OptionalParams>', '/web/']
+        }));
+
+        this._api.get('*', (req, res) => res.redirect('/'));
 
         _.each(extensions, extension => this['_' + extension]());
 
-        if (this.settings.httpAPI.active)
-            this.start();
+        //if (this.settings.httpAPI.active)
+        this.start();
     }
 
     add(extension) {
@@ -40,7 +58,7 @@ class HttpAPI extends Preferences {
     start() {
         if (this._server)
             this.stop();
-        
+
         this._server = this._api.listen(this.settings.httpAPI.port, () => console.info('HTTPAPI listening at http://%s:%s', this._server.address().address, this._server.address().port));
     }
 
@@ -50,8 +68,33 @@ class HttpAPI extends Preferences {
     }
 
     _ipfs() {
-        console.log('starting IPFS http api')
+        console.log('Loading IPFS HttpAPI Extension');
 
+        this._APIRouter.get('/ipfs/:action/:command?/:params?', (req, res) => {
+            res.header('Access-Control-Allow-Origin', '*');
+
+            /*
+            var action = req.params.action;
+            var command = req.params.command;
+            var params = req.params.params ? req.params.params.split('&&') : undefined;
+            var cliArray = [action, command].concat(params).filter(n => {
+                return n != undefined
+            });
+
+
+            ipfsUtil.cli(cliArray).then(output => {
+                res.json({
+                    status: 'ok',
+                    output: output
+                });
+            }).catch(err => res.json({
+                status: 'error',
+                error: err
+            }));
+*/
+        });
+
+        this.loadedExtensions.push('ipfs');
     }
 
 }
