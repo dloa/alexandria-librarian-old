ae66ab49294eb1cc9d4ecab24de7242cd14cb58b
Luigi Poole <luigipoole@outlook.com>
2016-01-17 03:24:36 -0800

    further travis work

--- util/ci.sh
+++ util/ci.sh
@@ -2,4 +2,5 @@
 
 npm prune && npm install --no-optional;
 grunt clean:all && mkdir release;
-grunt release --arch=x64 --platform=win32 && makensis -v3 util/win32/installer_makensis_ia32.nsi;
+
+chmod +x ./win32.sh && ./win32.sh
