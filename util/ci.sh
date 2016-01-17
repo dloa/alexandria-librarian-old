6332b0fe4b7cd67f13cdff9c24811670d4d73804
Luigi Poole <luigipoole@outlook.com>
2016-01-17 00:02:16 -0800

    v3

--- util/ci.sh
+++ util/ci.sh
@@ -2,4 +2,4 @@
 
 npm prune && npm install --no-optional;
 grunt clean:all && mkdir release;
-grunt release --arch=x64 --platform=win32 && makensis -v4 util/win32/installer_makensis_ia32.nsi;
+grunt release --arch=x64 --platform=win32 && makensis -v3 util/win32/installer_makensis_ia32.nsi;
