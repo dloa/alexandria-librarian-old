cfb6e279b6e09518ce3a24eebc46f76910ed869f
Luigi Poole <luigipoole@outlook.com>
2016-01-17 03:16:55 -0800

    ia32 build

--- util/win32.sh
+++ util/win32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grunt release --arch=x64 --platform=win32 && makensis -v3 ./win32/installer_makensis_ia32.nsi;
+grunt release --arch=ia32 --platform=win32 && makensis -v3 ./win32/installer_makensis_ia32.nsi;
